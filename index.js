79cdd67cb05a15cd51e47aab50602bce2fa3811a
surinder singh <surinder83singh@gmail.com>
2020-05-12 00:35:49 +0530

    rpc,app moved to flow-rpc, flow-app

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const utils = require("./lib/utils");
 const Router = require("./lib/router");
-const App = require("./lib/app");
-const RPC = require("./lib/rpc");
 
 module.exports = {
 	utils,
-	App, Router, RPC, 
-	FlowApp:App, FlowRouter:Router, FlowRPC:RPC
+	Router, 
+	FlowRouter:Router
 }

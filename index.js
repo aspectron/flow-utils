b562f3defba0e9949eade1ab7c7ca557a526388e
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2021-01-04 17:25:14 -0500

    fixing utils object property handling

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
+const isVertx = (typeof vertx !== 'undefined' && vertx !== null) ? true : false;
 const utils = require("./lib/utils");
+isVertx ? require('./lib/vertx')(utils) : require('./lib/node')(utils);
 module.exports = utils;
